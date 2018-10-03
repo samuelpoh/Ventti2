7306fc04c00254df147a29b1bc03ddc5bc4cd978
Merja <s1801210@edu.bc.fi>
2018-10-03 12:26:36 +0300

    Pelisäännöt on luotu

--- src/ventti2/Ventti2.java
+++ src/ventti2/Ventti2.java
@@ -16,11 +16,30 @@ public class Ventti2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hei tervetuloa pelaamaan");
-        System.out.println("Kerron sinulle pelin säännöt");
-        System.out.println("Pelaajana pelaat jakajaa vastaan.");
-        System.out.println("Pelaajana pelaat kätesi ensin valmiiksi ja pelin voittaa se jonka käsi on lähempänä arvoa 21.");
-        System.out.println("");
-    }
-    
+        System.out.println("Hei, \n" +
+                "\n" +
+                "tervetuloa pelaamaan!\n" +
+                "\n" +
+                "Kerron sinulle pelin säännöt:\n" +
+                "\n" +
+                "Pelaaja pelaa jakajaa vastaan. \n" +
+                "Pelaaja pelaa kätensä ensin valmiiksi ja pelin voittaa se jonka käsi on lähempänä arvoa 21.\n" +
+                "21 on korkein mahdollinen summa, sen ylittämällä häviää. \n" +
+                "\n" +
+                "Pelaaja pelaa käden ensin valmiiksi,  vasta sen jälkeen jakaja pelaa.\n" +
+                "Korttien 2-10 arvo on normaali, kuvakorttien arvot ovat jätkä = 11, kuningatar = 12, kuningas = 13, Ässä = 14 tai 1.\n" +
+                "\n" +
+                "Lisää: Uusi kortti.\n" +
+                "Stop: Ei enää uusia kortteja.\n" +
+                "\n" +
+                "Vuoro loppuu kun pelaaja lopettaa sen itse tai käsi menee yli 21:n(Hävisit). \n" +
+                "Pelaajan käsi on ventti jos siinä on viisikorttia vaikka arvo on alle 21.\n" +
+                "Ylimenon kohdalla pelaaja häviää, vaikka jakajan käsi menisi yli.\n" +
+                "Jakaja voittaa tasapeli tilanteessa.\n" +
+                "\n" +
+                "Kun pelaaja on pelannut, jakaja pelaa. \n" +
+                "Jakaja noudattaa samoja sääntöjä. \n" +
+                "Jakaja näyttää käden pelin aikana.");
+        
+}
 }
