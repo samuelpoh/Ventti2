6a161e3de040aed7fc5fc685897423ca8cd6bc53
Merja <s1801210@edu.bc.fi>
2018-10-03 12:31:48 +0300

    Lisätty tiedot korttipakkaan

--- src/ventti2/Korttipakka.java
+++ src/ventti2/Korttipakka.java
@@ -9,6 +9,80 @@ package ventti2;
  *
  * @author s1801210
  */
+
+    import java.util.ArrayList;
+    import java.util.List;
+
 public class Korttipakka {
+       
+        List<String> kortti = new ArrayList<String>();
+        
+		kortti.add;
+		
+                Hertta2.add("Hertta 2"); //0	
+		Hertta3.add("Hertta 3"); //1	
+		Hertta4.add("Hertta 4"); //2	
+		Hertta5.add("Hertta 5"); //3	
+		Hertta6.add("Hertta 6"); //4	
+		Hertta7.add("Hertta 7"); //5	
+		Hertta8.add("Hertta 8"); //6	
+		Hertta9.add("Hertta 9"); //7	
+		Hertta10.add("Hertta 10"); //8	
+		Hertta11.add("HErtta 11"); //9	
+		Hertta12.add("Hertta 12"); //10	
+		Hertta13.add("Hertta 13"); //11	
+		Hertta1.add("Hertta Ässä"); //12	
+				
+				
+		Ruutu2.add("Ruutu 2"); //13	
+		Ruutu3.add("Ruutu 3"); //14	
+		Ruutu4.add("Ruutu 4"); //15	
+		Ruutu5.add("Ruutu 5"); //16	
+		Ruutu6.add("Ruutu 6"); //17	
+		Ruutu7.add("Ruutu 7"); //18	
+		Ruutu8.add("Ruutu 8"); //19	
+		Ruutu9.add("Ruutu 9"); //20	
+		Ruutu10.add("Ruutu 10"); //21	
+		Ruutu11.add("Ruutu 11"); //22	
+		Ruutu12.add("Ruutu 12"); //23	
+		Ruutu13.add("Ruutu 13"); //24	
+		Ruutu1.add("Ruutu Ässä"); //25	
+				
+		Risti2.add("Risti 2"); //26	
+		Risti3.add("Risti 3"); //27	
+		Risti4.add("Risti 4"); //28	
+		Risti5.add("Risti 5"); //29	
+		Risti6.add("Risti 6"); //30	
+		Risti7.add("Risti 7"); //31	
+		Risti8.add("Risti 8"); //32	
+		Risti9.add("Risti 9"); //33	
+		Risti10.add("Risti 10"); //34	
+		Risti11.add("Risti 11"); //35	
+		Risti12.add("Risti 12"); //36	
+		Risti13.add("Risti 13"); //37	
+		Risti1.add("Risti Ässä"); //38	
+				
+		Pata2.add("Pata 2"); //39	
+		Pata3.add("Pata 3"); //40	
+		Pata4.add("Pata 4"); //41	
+		Pata5.add("Pata 5"); //42	
+		Pata6.add("Pata 6"); //43	
+		Pata7.add("Pata 7"); //44	
+		Pata8.add("Pata 8"); //45	
+		Pata9.add("Pata 9"); //46	
+		Pata10.add("Pata 10"); //47	
+		Pata11.add("Pata 11"); //48	
+		Pata12.add("Pata 12"); //49	
+		Pata13.add("Pata 13"); //50	
+		Pata1.add("Pata Ässä"); //51	
+
+        
+            
+		                      
+                for(int i=0;i<kortti.size();i==){
+			System.out.println(kortti.get(i));
+                }
+		}
+	}
     
 }
