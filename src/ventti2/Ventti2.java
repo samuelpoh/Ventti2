c04ebc51fff7c05a440b09c56553b4d2c8d79a39
Samuel Pohjolainen <samuel.pohjolainen@edu.bc.fi>
2018-10-03 12:16:03 +0300

    Pelin säännöt

--- src/ventti2/Ventti2.java
+++ src/ventti2/Ventti2.java
@@ -16,6 +16,8 @@ public class Ventti2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hei tervetuloa pelaamaan");
+        System.out.println("Kerron sinulle pelin säännöt");
     }
     
 }
