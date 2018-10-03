abe1ca2e3030537196240942cda77106aa154d06
Merja <s1801210@edu.bc.fi>
2018-10-03 12:12:30 +0300

    Lisätään kortti olio

---
+++ src/ventti2/Kortti.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventti2;
+
+/**
+ *
+ * @author s1801210
+ */
+public enum Kortti {
+    
+}
