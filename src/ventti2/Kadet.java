da11ece32aca3162c245c8f1ef9953bc829e6aac
Merja <s1801210@edu.bc.fi>
2018-10-04 09:42:55 +0300

    Kädet luokkaa muokattu

--- src/ventti2/Kadet.java
+++ src/ventti2/Kadet.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ventti2;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author s1801210
  */
-public class Kadet {
-    
+        public class Kadet {
+            private ArrayList<Peli> kadet;
+            
+        public void addPeli(Peli c){
+            this.kadet.add(c);
 }
+   }
+         
